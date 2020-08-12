2b03f8004737f982a7913cd9fe5cac179d0021f5
GishaTheDeveloper <spaceshhz@gmail.com>
2020-08-12 23:45:57 +0300

    Basic Movement System (WIP)

---
+++ Shooter Game/Assets/Scripts/FPSCameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FPSCameraController : MonoBehaviour
+{
+    public Transform body;
+
+    public float mouseSensitivity;
+
+    float xRot, yRot;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        // Camera Input.
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Camera/Body Rotating.
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+
+        yRot = mouseX;
+        body.Rotate(Vector3.up * yRot);
+    }
+}
---
+++ Shooter Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Controller")]
+    private CharacterController controller;
+    public const float gravity = -9.81f;
+
+    public float walkSpeed = 7f;
+    public float sprintSpeed = 7f;
+    [Space]
+    public float jumpForce = 7f;
+    public float gravityMultiplier = 2.5f;
+
+    [Header("Ground Checker")]
+    public Transform groundChecker;
+    public float checkerRadius;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    bool isSprinting;
+
+    float horizontalAxis;
+    float verticalAxis;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovementInput();
+    }
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void MovementInput()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            Jump();
+
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+    }
+
+    void Movement()
+    {
+        isGrounded = Physics.CheckSphere(groundChecker.position, checkerRadius, groundMask);
+
+        // Grounded Physics.
+        if (isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+            controller.slopeLimit = 45.0f;
+        }
+
+        float moveSpeed = !isSprinting ? walkSpeed : sprintSpeed;
+        Vector3 movement = transform.right * horizontalAxis + transform.forward * verticalAxis;
+        controller.Move(movement * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * gravityMultiplier * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        controller.slopeLimit = 90f;
+    }
+}
